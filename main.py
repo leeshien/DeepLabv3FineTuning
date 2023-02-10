49fcb39602bee6d6023691d5665488eee9f3329c
leeshien <ganleeshien@gmail.com>
2023-02-11 01:18:02 +0800

    add additional args

--- main.py
+++ main.py
@@ -11,9 +11,18 @@ from trainer import train_model
 
 
 @click.command()
-@click.option("--data-directory",
+@click.option("--train-data-directory",
               required=True,
-              help="Specify the data directory.")
+              help="Specify the main train data directory.")
+@click.option("--test-data-directory",
+              required=True,
+              help="Specify the main test data directory.")
+@click.option("--image-folder",
+              required=True,
+              help="Specify the image folder name.")
+@click.option("--mask-folder",
+              required=True,
+              help="Specify the mask folder name.")
 @click.option("--exp_directory",
               required=True,
               help="Specify the experiment directory.")
@@ -26,12 +35,13 @@ from trainer import train_model
               default=4,
               type=int,
               help="Specify the batch size for the dataloader.")
-def main(data_directory, exp_directory, epochs, batch_size):
+def main(train_data_directory, test_data_directory, image_folder, mask_folder, exp_directory, epochs, batch_size):
     # Create the deeplabv3 resnet101 model which is pretrained on a subset
     # of COCO train2017, on the 20 categories that are present in the Pascal VOC dataset.
     model = createDeepLabv3()
     model.train()
-    data_directory = Path(data_directory)
+    train_data_directory = Path(train_data_directory)
+    test_data_directory = Path(test_data_directory)
     # Create the experiment directory if not present
     exp_directory = Path(exp_directory)
     if not exp_directory.exists():
@@ -48,7 +58,7 @@ def main(data_directory, exp_directory, epochs, batch_size):
 
     # Create the dataloader
     dataloaders = datahandler.get_dataloader_single_folder(
-        data_directory, batch_size=batch_size)
+        train_data_directory, test_data_directory, image_folder, mask_folder, batch_size=batch_size)
     _ = train_model(model,
                     criterion,
                     dataloaders,
