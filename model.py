2b09ef5fcdf2033483472b7ee86b880f304b1aad
leeshien <ganleeshien@gmail.com>
2023-02-21 15:24:39 +0800

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from torchvision.models.segmentation.deeplabv3 import DeepLabHead
 from torchvision import models
 
 
-def createDeepLabv3(outputchannels=1):
+def createDeepLabv3(backbone, outputchannels=1):
     """DeepLabv3 class with custom head
 
     Args:
@@ -13,8 +13,14 @@ def createDeepLabv3(outputchannels=1):
     Returns:
         model: Returns the DeepLabv3 model with the ResNet101 backbone.
     """
+    if backbone == 'mobilenetv3':
+        model = models.segmentation.deeplabv3_mobilenet_v3_large(pretrained=True, progress=True)
+    elif backbone == 'resnet50':
+        model = models.segmentation.deeplabv3_resnet50(pretrained=True, progress=True)
+    elif backbone == 'resnet101':
+            model = models.segmentation.deeplabv3_resnet101(pretrained=True, progress=True)    
 #     model = models.segmentation.deeplabv3_resnet101(pretrained=True, progress=True)
-    model = models.segmentation.deeplabv3_resnet50(pretrained=True, progress=True)
+#     model = models.segmentation.deeplabv3_resnet50(pretrained=True, progress=True)
     
     model.classifier = DeepLabHead(2048, outputchannels)
     # Set the model in training mode
